f314fc6cea37c7487fa558a2e7ff06b9e23397c4
♠ ace hacker <ryan@heroku.com>
2012-09-08 12:56:15 -0700

    init

---
+++ main.rb
@@ -0,0 +1,14 @@
+$stdout.sync = true
+
+def log(data)
+  data = {app: "l2met-canary"}.merge(data)
+  data.reduce(out=String.new) do |s, tup|
+    s << [tup.first, tup.last].join("=") << " "
+  end
+  puts(out)
+end
+
+loop do
+  sleep(1)
+  log(measure: true, at: "canary-test")
+end
